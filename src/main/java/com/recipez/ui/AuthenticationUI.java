fc790b5d9d07694bc853e1152b250d6156674654
David Baez <david.baez456@gmail.com>
2026-05-05 15:25:20 -0700

    html logos

--- src/main/java/com/recipez/ui/AuthenticationUI.java
+++ src/main/java/com/recipez/ui/AuthenticationUI.java
@@ -25,8 +25,8 @@ import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Image;
 import java.awt.Insets;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+//import java.awt.event.ActionEvent;
+//import java.awt.event.ActionListener;
 import javax.swing.ImageIcon;
 
 public class AuthenticationUI extends JPanel {
--- src/main/java/com/recipez/ui/DashboardUI.java
+++ src/main/java/com/recipez/ui/DashboardUI.java
@@ -22,7 +22,7 @@ import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
-import javax.swing.SwingConstants;
+//import javax.swing.SwingConstants;
 import javax.swing.SwingUtilities;
 import java.awt.*;
 import java.awt.event.ActionListener;
